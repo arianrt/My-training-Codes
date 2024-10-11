ac6e3b0875e4abf14390ec46a278020124e724ce
arianrt <arianramezani.t@gmail.com>
2024-10-11 17:09:25 +0330

    Add files via upload

---
+++ Training/main.js
@@ -0,0 +1,8 @@
+// $(document).ready(function () {
+//     setTimeout( function(){$("#card1").css("opacity", "100%");}, 1000)
+//     setTimeout( function(){$("#card2").css("opacity", "100%");}, 1500)
+//     setTimeout( function(){$("#card3").css("opacity", "100%");}, 2000)
+//     setTimeout( function(){$("#card4").css("opacity", "100%");}, 2500)
+//     setTimeout( function(){$("#card5").css("opacity", "100%");}, 3000)
+//     setTimeout( function(){$("#card6").css("opacity", "100%");}, 3500)
+// });
